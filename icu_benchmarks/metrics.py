34cbdf86484c5484571ba3791ed3ec9c27939bee
Malte Londschien (euler) <malte@londschien.de>
2025-02-16 13:48:50 +0100

    Track quantiles of residuals for regression task.

--- icu_benchmarks/metrics.py
+++ icu_benchmarks/metrics.py
@@ -31,16 +31,24 @@ def metrics(y, yhat, prefix, task):  # noqa D
             ),
         }
     elif task == "regression":
-        abs_residuals = np.abs(y - yhat)
-        quantiles = np.quantile(abs_residuals, [0.8, 0.9, 0.95])
+        residuals = y - yhat
+        quantiles = np.quantile(residuals, [0.1, 0.25, 0.5, 0.75, 0.9])
+        abs_residuals = np.abs(residuals)
+        abs_quantiles = np.quantile(abs_residuals, [0.8, 0.9, 0.95])
         mse = np.mean(abs_residuals**2)
         return {
             f"{prefix}mse": mse,
             f"{prefix}rmse": np.sqrt(mse),
             f"{prefix}mae": np.mean(abs_residuals),
-            f"{prefix}quantile_0.8": quantiles[0],
-            f"{prefix}quantile_0.9": quantiles[1],
-            f"{prefix}quantile_0.95": quantiles[2],
+            f"{prefix}abs_quantile_0.8": abs_quantiles[0],
+            f"{prefix}abs_quantile_0.9": abs_quantiles[1],
+            f"{prefix}abs_quantile_0.95": abs_quantiles[2],
+            f"{prefix}quantile_0.1": quantiles[0],
+            f"{prefix}quantile_0.25": quantiles[1],
+            f"{prefix}quantile_0.5": quantiles[2],
+            f"{prefix}quantile_0.75": quantiles[3],
+            f"{prefix}quantile_0.9": quantiles[4],
+            f"{prefix}mean_residual": np.mean(residuals),
         }
     else:
         raise ValueError(f"Unknown task {task}")
